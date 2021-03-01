9035138bf0dcdafdc130e866c95cdf5930869e34
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-02-28 16:45:52 -0800

    created login and logout pages

--- src/pages/newgame.js
+++ src/pages/newgame.js
@@ -1,34 +1,81 @@
-import React from "react";
-import store from "../config/store"
+import React, { useState } from "react";
+import store from "../config/store";
+import API from "../utils/API";
 
-// handleChange(event) {
-//     let inputVal = event.target.value
-//     console.log(inputVal)
-//    this.setState({value: inputVal});
-//  }
-// handleSubmit(event) {
-//     alert('A name was submitted: ' + this.state.value);
-//     event.preventDefault();
-//   }
-function NewGame(){
-    
-        return (
-          <form>
-            <label>
-              User:
+function NewGame() {
+  // Set initial user state
+  const [userState, setUserState] = useState({
+    username: "",
+    password: "",
+    token: "",
+    isLoggedIn: false
+  });
+
+  // Set initial signup state
+  const [signupState, setSignupState] = useState({
+    username: "",
+    password: ""
+  })
+
+  // Set signup state on input change
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setSignupState({
+      ...signupState,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = e=>{
+    e.preventDefault();
+    API.signup(signupState).then(res=>{
+      console.log(`Congrats! ${JSON.stringify(res.data)}`);
+      localStorage.setItem("token",res.data.token)
+      setUserState({
+        username: res.data.username,
+        password: res.data.password,
+        token: res.data.token,
+        isLoggedIn: true
+      });
+      store.dispatch({
+        type: "USER_ACTION",
+        payload: {
+          ...userState
+        }
+      });
+      setSignupState({
+        email:"",
+        password:"",
+        name:''
+      });
+    }).catch(err=>{
+      console.log(`FOOL! Due to your stupidity, ${err}`);
+      store.dispatch({
+        type: "USER_ACTION",
+        payload: {
+          username: "",
+          password: "",
+          token: "",
+          isLoggedIn: false
+        }
+      });
+      localStorage.removeItem("token")
+    })
+  }
+
+  return (
+    <form>
+      <label>
+        User:
             </label>
-              <input name ="userAns" type="text" />
-            <input type="submit" value="Submit" />
-            <label>
-              Password:
+      <input name="username" type="text" onChange={handleInputChange} />
+      <label>
+        Password:
             </label>
-              <input name ="userAns" type="text" />
-            <input type="submit" value="Submit" />
-          </form>
-        );
+      <input name="password" type="password" onChange={handleInputChange}/>
+      <input type="submit" value="Submit" onClick={handleSubmit} />
+    </form>
+  );
 }
-    
-  
-
 
 export default NewGame
