815740ae804359f753d1823c1d0e60cfd8c49096
Bethany Fulcher <fulcherbethany@gmail.com>
2021-03-02 11:15:20 -0800

    added switch case to algorithms

--- src/components/Forms/CodeEditor.js
+++ src/components/Forms/CodeEditor.js
@@ -25,75 +25,115 @@ function Editor() {
         });
     };
 
+    function resetKeys() {
+        store.dispatch({
+            type: "ADD_KEY",
+            payload: {
+                ...store.getState().key, amount: 0
+            }
+        })
+    }
+
+    function showWinDialogue() {
+        store.dispatch({
+            type: "SHOW_MODAL",
+            payload: {
+                ...store.getState().modal, dialogue: store.getState().modal.winDialogue
+            }
+        })
+    }
+
+    function showWrongDialogue() {
+        store.dispatch({
+            type: "SHOW_MODAL",
+            payload: {
+                ...store.getState().modal, dialogue: store.getState().modal.wrongDialogue
+            }
+        })
+    }
+
     // On submit, grab text from code editor
     const onSubmit = event => {
         event.preventDefault();
+        const level = store.getState().user.level
+        switch (level) {
+            case 1:
+                // API call for easy algorithm, parse arguments and output
+                API.easyAlgo().then(res => {
+                    const info = JSON.parse(res.data.argsAndOutput);
+                    // Create and call a new function to run the code written into the editor
+                    const testFunction = new Function("str", editorState.editorText);
+                    const result = testFunction(info.args)
+                    console.log(`Here's what we get back: ${result}`);
+                    console.log(`Here's what we expect: ${info.output}`);
 
-        // API call for easy algorithm, parse arguments and output
-        API.easyAlgo().then(res => {
-            const info = JSON.parse(res.data.argsAndOutput);
+                        if (result === info.output) {
+                            resetKeys()
+                            showWinDialogue()
+                            history.push("/winscreen")
+                        }
+                        else {
+                            showWrongDialogue()
+                        }
+                }).catch(err => {
+                    showWrongDialogue()
+                    console.log(`Due to your idiocy, ${err}`);
+                })
+                break;
+            case 2:
+                // API call for easy algorithm, parse arguments and output
+                API.medAlgo().then(res => {
+                    const info = JSON.parse(res.data.argsAndOutput);
+                    // Create and call a new function to run the code written into the editor
+                    const testFunction = new Function("str", editorState.editorText);
+                    const result = testFunction(info.args)
+                    console.log(`Here's what we get back: ${result}`);
+                    console.log(`Here's what we expect: ${info.output}`);
 
-            // Create and call a new function to run the code written into the editor
-            const testFunction = new Function("str", editorState.editorText);
-            const result = testFunction(info.args)
-            console.log(`Here's what we get back: ${result}`);
-            console.log(`Here's what we expect: ${info.output}`);
+                        if (result === info.output) {
+                            resetKeys()
+                            showWinDialogue()
+                            history.push("/winscreen")
+                        }
+                        else {
+                            showWrongDialogue()
+                        }
+                }).catch(err => {
+                    showWrongDialogue()
+                    console.log(`Due to your idiocy, ${err}`);
+                })
+                break;
+            case 3:
+                // API call for easy algorithm, parse arguments and output
+                API.hardAlgo().then(res => {
+                    const info = JSON.parse(res.data.argsAndOutput);
+                    // Create and call a new function to run the code written into the editor
+                    const testFunction = new Function("str", editorState.editorText);
+                    const result = testFunction(info.args)
+                    console.log(`Here's what we get back: ${result}`);
+                    console.log(`Here's what we expect: ${info.output}`);
 
-            // API call for NPCs
-            API.allNPC().then(data => {
-                const luther = data.data[1]
-                // Luther will evaluate your answer and judge you
-                if (result === info.output) {
-                    store.dispatch({
-                        type: "ADD_KEY",
-                        payload: {
-                            ...store.getState().key, amount: 0
+                    // API call for NPCs
+                    API.allNPC().then(data => {
+                        // Luther will evaluate your answer and judge you
+                        if (result === info.output) {
+                            resetKeys()
+                            showWinDialogue()
+                            history.push("/winscreen")
+                        }
+                        else {
+                            showWrongDialogue()
                         }
                     })
-                    store.dispatch({
-                        type: "SHOW_MODAL",
-                        payload: {
-                            ...store.getState().modal, dialogue: luther.usefulDialogue[3]
-                        }
-                    })
-                    history.push("/winscreen")
-                }
-                else {
-                    // store.dispatch({
-                    //     type: "ADD_KEY",
-                    //     payload: {
-                    //         ...store.getState().key, amount: 0
-                    //     }
-                    // })
-                    store.dispatch({
-                        type: "SHOW_MODAL",
-                        payload: {
-                            ...store.getState().modal, dialogue: luther.usefulDialogue[4]
-                        }
-                    })
- 
-                    // history.push("/")
-                }
+                }).catch(err => {
+                    showWrongDialogue()
+                    console.log(`Due to your idiocy, ${err}`);
+                })
+                break;
 
-            })
-        }).catch(err => {
-            // store.dispatch({
-            //     type: "ADD_KEY",
-            //     payload: {
-            //         ...store.getState().key, amount: 0
-            //     }
-            // })
-
-            store.dispatch({
-                type: "SHOW_MODAL",
-                payload: {
-                    ...store.getState().modal, dialogue: store.getState().modal.wrongDialogue
-                }
-            })
-
-            console.log(`Due to your idiocy, ${err}`);
-            // history.push("/")
-        })
+            default:
+                break;
+        }
     }
 
 
