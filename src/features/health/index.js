ee1b373a09e87ea7f6343279b68ee3949ccf9e05
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-25 12:43:27 -0700

    health back to function, fixed map state to props issues

--- src/features/health/index.js
+++ src/features/health/index.js
@@ -4,44 +4,35 @@ import store from '../../config/store';
 import empty from './empty.png'
 import filled from './filled.png'
 
-class Health extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { health: 0 }
+function Health(props) {
+
+    const health = props.health;
+    console.log(health);
+    switch (health) {
+        case 0:
+            document.querySelector('.health1').src = empty
+            document.querySelector('.health2').src = empty
+            document.querySelector('.health3').src = empty
+        case 1:
+            document.querySelector('.health1').src = filled
+            break;
+        case 2:
+            document.querySelector('.health1').src = filled
+            document.querySelector('.health2').src = filled
+            break;
+        default:
+            break;
     }
 
-    componentDidMount() {
-        const health = this.props.health;
-        console.log(health);
-        switch (health) {
-            case 1:
-                document.querySelector('.health1').src = filled
-                break;
-            case 2:
-                document.querySelector('.health1').src = filled
-                document.querySelector('.health2').src = filled
-                break;
-            case 3:
-                document.querySelector('.health1').src = filled
-                document.querySelector('.health2').src = filled
-                document.querySelector('.health3').src = filled
-                break;
-            default:
-                document.querySelector('.health1').src = empty
-                document.querySelector('.health2').src = empty
-                document.querySelector('.health3').src = empty
-        }
-    }
 
-    render() {
-        return (
-            <div className={'health'}>
-                <img className='health health1' src={filled} />
-                <img className='health health2' src={filled} />
-                <img className='health health3' src={filled} />
-            </div>
-        )
-    }
+
+    return (
+        <div className={'health'}>
+            <img className='health health1' src={filled} />
+            <img className='health health2' src={filled} />
+            <img className='health health3' src={filled} />
+        </div>
+    )
 
 }
 
