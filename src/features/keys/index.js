42a184468946cfc3fe814d098d8579bbe8c08b47
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-26 14:59:53 -0700

    added necessary cases to health and key files

--- src/features/keys/index.js
+++ src/features/keys/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import store from '../../config/store'
 import empty from './empty.png'
@@ -9,34 +9,43 @@ import filled from './filled.png'
 import './style.css'
 
 
- function Keys(props) {
+function Keys(props) {
 
-    const amount = props.amount
-    console.log(amount)
-    switch (amount) {
-        case 1:
-            document.querySelector('.key1').src=filled
+    useEffect(() => {
+        const amount = props.amount
+        console.log(amount)
+        switch (amount) {
+            case 0:
+                document.querySelector('.key1').src = empty
+                document.querySelector('.key2').src = empty
+                document.querySelector('.key3').src = empty
             break;
+            case 1:
+                document.querySelector('.key1').src = filled
+                break;
 
-        case 2:
-            document.querySelector('.key1').src=filled
-            document.querySelector('.key2').src=filled
-            break;
+            case 2:
+                document.querySelector('.key1').src = filled
+                document.querySelector('.key2').src = filled
+                break;
 
-        case 3:
-            document.querySelector('.key1').src=filled
-            document.querySelector('.key2').src=filled
-            document.querySelector('.key3').src=filled
-            break;
-    
-        default:
-            break;
-    }
+            case 3:
+                document.querySelector('.key1').src = filled
+                document.querySelector('.key2').src = filled
+                document.querySelector('.key3').src = filled
+                break;
+
+            default:
+                break;
+        }
+
+    })
+
     return (
         <div className={'keys'}>
-            <img className="key key1" src={empty}/>
-            <img className="key key2" src={empty}/>
-            <img className="key key3" src={empty}/>
+            <img className="key key1" src={empty} />
+            <img className="key key2" src={empty} />
+            <img className="key key3" src={empty} />
 
         </div>
     )
@@ -44,9 +53,9 @@ import './style.css'
 
 function mapStateToProps(state) {
     return {
-        ...state.key, 
+        ...state.key,
 
     }
-} 
+}
 
 export default connect(mapStateToProps)(Keys)
