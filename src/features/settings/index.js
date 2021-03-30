0c668efff4fe18e0d4613a36bdc554c92f24ea8a
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-03-30 10:49:23 -0700

    removed lose progress message

--- src/features/settings/index.js
+++ src/features/settings/index.js
@@ -23,20 +23,19 @@ export default function Sound() {
         history.push('/')
     }
 
-    return(
+    return (
         <div>
 
-        <button onClick={handleHomeButton}><img className="home_button" src={home_button}/></button>
-        
-        <div className="setting_wrapper">
-        {/* modal when home button is clicked on */}
-        <div className=" rpgui-center settings_modal rpgui-container framed" style={{display: "none"}}>
-            <h1 style={{ fontSize: '250%', margin: 'auto'}}>Are you sure you want to quit? </h1>
-            <h1>You will lose all progress for your current level.</h1>
-            <button type="button" className="settings_button rpgui-button" onClick={handleGoHome}>Go home</button>
-            <button type="button" className="setting_button rpgui-button" onClick={handleClose}>Close</button>
-        </div>
-        </div>
+            <button onClick={handleHomeButton}><img className="home_button" src={home_button} /></button>
+
+            <div className="setting_wrapper">
+                {/* modal when home button is clicked on */}
+                <div className=" rpgui-center settings_modal rpgui-container framed" style={{ display: "none" }}>
+                    <h1 style={{ fontSize: '250%', margin: 'auto' }}>Are you sure you want to quit?</h1>
+                    <button type="button" className="settings_button rpgui-button" onClick={handleGoHome}>Go home</button>
+                    <button type="button" className="setting_button rpgui-button" onClick={handleClose}>Close</button>
+                </div>
+            </div>
 
         </div>
     )
